f56e8ebcd6b087da317b4593bcee8d776d361094
richarddeanpelser <pelserricharddean@gmail.com>
2022-05-16 10:42:33 +0100

    commit

--- core/settings.py
+++ core/settings.py
@@ -121,12 +121,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'portfolio/static')
 STATIC_URL = 'static/'
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'templates')
 STATICFILES_DIRS = [
-    BASE_DIR / 'portfolio/templates/',
+    # BASE_DIR / 'portfolio/static',
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
