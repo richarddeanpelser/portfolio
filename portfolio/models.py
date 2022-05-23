a69b8ee9837044be681afe300be27403ba0fd773
richarddeanpelser <pelserricharddean@gmail.com>
2022-05-23 15:06:52 +0100

    commit

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,8 +1,8 @@
-from statistics import mode
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.utils.text import slugify
 
 ROLE_STATUS = (
     (0,"Active"),
@@ -14,6 +14,16 @@ PROJECT_STATUS = (
     (1,"Completed")
 )
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4,  unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
+
 class Role(models.Model):
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
@@ -23,6 +33,20 @@ class Role(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if self.slug == None:
+            slug = slugify(self.title)
+            has_slug = Role.objects.filter(slug=slug).exists()
+            count = 1
+            while has_slug:
+                count += 1
+                slug = slugify(self.title) + '-' + str(count)
+                has_slug = Role.objects.filter(slug=slug).exists()
+                self.slug = slug
+
+        super().save(*args, **kwargs)
+
 
 
 class Project(models.Model):
@@ -35,9 +59,24 @@ class Project(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4,  unique=True,
                           primary_key=True, editable=False)
+    tags = models.ManyToManyField(Tag, blank=True)                      
 
     def __str__(self):
-        return self.title        
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if self.slug == None:
+            slug = slugify(self.title)
+            has_slug = Project.objects.filter(slug=slug).exists()
+            count = 1
+            while has_slug:
+                count += 1
+                slug = slugify(self.title) + '-' + str(count)
+                has_slug = Project.objects.filter(slug=slug).exists()
+                self.slug = slug
+
+        super().save(*args, **kwargs)
+        		
 
 class Skill(models.Model):
     title = models.CharField(max_length=200)
@@ -50,14 +89,6 @@ class Skill(models.Model):
     def __str__(self):
         return self.title
 
-class Tag(models.Model):
-    name = models.CharField(max_length=200, null=True)
-    created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4,  unique=True,
-                          primary_key=True, editable=False)
-
-    def __str__(self):
-        return self.name
 
 
 # class Project(models.Model):
